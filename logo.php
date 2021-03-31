3d70a0255b9f86d85e6e9e106c64446098acf854
Tangui Morlier <tangui@tangui.eu.org>
2021-04-01 00:14:48 +0200

    Logo par défaut

--- logo.php
+++ logo.php
@@ -1,4 +1,7 @@
 <?php
 $region = array_shift(explode('.', preg_replace('/https.../', '', $_GET['service']) ));
 $region = preg_replace('/[^0-9a-z]/', '', $region);
+if (!$region) {
+	$region = 'confederation';
+}
 header('Location: /images/logos/logo_igp'.$region.'.png');
