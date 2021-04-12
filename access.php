4ce5f9629421a21233a15c7cd3baacc7499131dd
Tangui Morlier <tangui@tangui.eu.org>
2021-04-12 13:28:09 +0200

    Permet l'accès au mot de passe oublié et à la création de compte

---
+++ access.php
@@ -0,0 +1,17 @@
+<?php
+
+$service = $_GET['service'];
+$host = explode('/', $service)[2];
+
+if ($host) {
+    switch ($_GET['action']) {
+        case 'mdpoublie':
+            header("Location: https://".$host."/teledeclarant/code_creation");
+            exit;
+        case 'creation':
+            header("Location: https://".$host."/mot_de_passe_oublie");
+            exit;
+    }
+}
+header("Location: https://login.igp.vins.24eme.fr/?service=".$service);
+exit;
