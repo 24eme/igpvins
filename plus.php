4f82e53b9c31867ff72566031a40e90671849333
Tangui Morlier <tangui@tangui.eu.org>
2021-09-23 11:38:53 +0200

    Ancre pour type de documents

--- plus.php
+++ plus.php
@@ -91,7 +91,7 @@ $documents = $igp_documents[$_GET['igp']];
                 </div>
                 <?php endif; ?>
                 <div class="col-sm-12">
-                <h2><?php echo $actualtitle; ?></h2>
+                <h2 id="<?php echo str_replace(" ", "_", strtolower($actualtitle)); ?>"><?php echo $actualtitle; ?></h2>
                 <ul>
               <?php endif; ?>
               <li><?php echo preg_replace('/(\d+)-(\d+)-(\d+)/', '$3/$2/$1', $doc[1]); ?> : <a href="documents/<?php echo $_GET['igp'].'/'.$doc[3]?>"><?php echo $doc[2]; ?></a></li>
