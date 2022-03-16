b2a27bec16dd6c7456a5b428e6ecc6f93fa42738
Tangui Morlier <tangui@tangui.eu.org>
2022-03-16 18:28:57 +0100

    Gestion des logos lorsqu'on vient de viticonnect

--- logo.php
+++ logo.php
@@ -1,6 +1,9 @@
 <?php
 $region = array_shift(explode('.', preg_replace('/https.../', '', $_GET['service']) ));
 $region = preg_replace('/[^0-9a-z]/', '', $region);
+if ($region == 'viticonnect') {
+        $region = str_replace('igp', '', strtolower(array_pop(explode('/', $_GET['service']))));
+}
 if (!$region) {
 	$region = 'confederation';
 }
