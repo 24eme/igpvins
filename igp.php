d44158689c570399c90b17c5d58e16f14dd94cb5
Tangui Morlier <tangui@tangui.eu.org>
2025-07-24 14:56:22 +0200

    FRIGP

--- igp.php
+++ igp.php
@@ -38,8 +38,8 @@ $igp_contacts = array(
         "portail"=>"https://www.innov-agro.igpvins.fr/",  "logo"=> "logo_igpcharentais.png"
     ),
     "comtetolosan" => array (
-        "title" => "Syndicat des Vins IGP Comté Tolosan - FRIGP Sud-Ouest",
-        "adresse" => "FRIGP<br/> BP 82256 – Auzeville<br/>31322 Castanet Tolosan",
+        "title" => "Fédération Régionale des vins IGP du Sud-Ouest<br/><small>(Ariège, Aveyron, Comté Tolosan, Lavilledieu, Thezac Perricard)</small>",
+        "adresse" => "BP 82256 – Auzeville<br/>31322 Castanet Tolosan",
         "contact" => "05.61.75.42.82<br/>s.arenes@coopoccitanie.fr<br/><b>Contact:</b> Sandrine ARENES DABAN<br/><b>Président :</b> Christophe Bou<br/>",
         "portail"=>"https://sudouest.igp.vins.24eme.fr/",  "logo"=> "logo_igpsudouest.png"
 
