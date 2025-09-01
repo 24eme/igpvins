931c8c7d5ff364d99c0cd111869c681c32a430ba
Tangui Morlier <tangui@tangui.eu.org>
2025-09-01 11:33:22 +0200

    Prépération du multi domaine

---
+++ config_igp.php
@@ -0,0 +1,131 @@
+<?php
+$igp_contacts = array(
+    "allobrogies" => array (
+        "title" => "Syndicat des Vins de Pays Allobrogies",
+        "adresse" => "40 rue du Terraillet<br/><br/>73190 SAINT BALDOPH",
+        "contact" => "04 79 33 82 42<br/>emmanuel.michaud@fdsea73.fr<br/><br/><b>Contact:</b>Emmanuel MICHAUD<br/><b>Président:</b>Jean-Paul FINAS<br/>",
+        "portail" => "https://www.innov-agro.igpvins.fr/", "logo" => "logo_igpallobrogies.png"
+    ),
+    "alpesdusud" => array(
+        "title" => "Syndicat des Vins des Alpes du Sud",
+        "adresse" => "2 rue Osco Manosco<br/><br/>04860 PIERREVERT",
+        "contact" => "04 90 12 45 20<br/>direction@vins-des-alpes-du-sud.fr<br/><br/><b>Contact : </b>Mladen GRUMIC<br/><b>Président AOP :</b> Gilles DELSUC<br/><b>Président IGP :</b>  Frédéric PORT<br/>",
+        "portail"=>"https://alpes.igp.vins.24eme.fr/",  "logo"=> "logo_igpalpes.png"
+    ),
+    "ardeche" =>  array (
+        "title" => "Syndicat des Vins IGP d′Ardeche",
+        "adresse" => "Maison des Vignerons<br />218 Route de Ruoms<br/><br/>07150 VALLON PONT D'ARC",
+        "contact" => "04 75 93 90 13<br/>svdp-fcca@wanadoo.fr<br/><a href='http://www.lesvinsdardeche.com'>Site web</a><br/><b>Contact:</b> Catherine JURY - Anne MOISAN<br/><b>Président :</b> Marc DEJOUX<br/>",
+        "portail"=>"https://ardeche.igp.vins.24eme.fr/",  "logo"=> "logo_igpardeche.png"
+    ),
+    "aude" => array (
+        "title" => "Union Syndicale des IGP de l′Aude",
+        "adresse" => "Maison des Vignerons - ZAC Bonne Source<br/>22 rue Ernest Cognacq - BP 40527<br/>11105 NARBONNECedex",
+        "contact" => "04.68.90.22.29<br/>l.zorzettig@vignerons.com<br/><br/><b>Contact:</b> Laurence ZORZETTIG<br/><b>Président:</b> Ludovic ROUX<br/>",
+        "portail"=>"https://www.innov-agro.igpvins.fr/", "logo" => "logo_igpaude.png"
+    ),
+    "13" => array (
+        "title" => "Syndicat des vins IGP des Bouches-du-Rhone",
+        "adresse" => "Maison des Agriculteurs<br/>22 Avenue Henri-Pontier<br/>13626 AIX EN PROVENCE Cedex 1",
+        "contact" => "06.23.90.24.41<br/>igp13@orange.fr<br/><br/><b>Contact:</b> Marie DE MONTE<br/><b>Président:</b> Jean-Claude PELLEGRIN<br/>",
+        "portail"=> array('Antenne d\'Aix' => "https://13.igp.vins.24eme.fr/", 'Antenne d\'Arles' => "https://arles.igp.vins.24eme.fr/"),  "logo" => "logo_igp13.png",
+        "url_plusdinfo" => "https://www.igp13vins.com/documents-officiels-2.html",
+    ),
+    "charentais" => array (
+        "title" => "Syndicat des Vins de Pays Charentais",
+        "adresse" => "23 allees Bernard Guionnet - BP 90018<br/><br/>16101 COGNAC CEDEX",
+        "contact" => "05 17 22 00 00<br/>info@vindepayscharentais.fr<br/><a href='http://www.vindepayscharentais.fr'>Site web</a><br/><b>Contact:</b> Julie FOUASSIER<br/><b>Président:</b> Thierry JULLION<br/>",
+        "portail"=>"https://www.innov-agro.igpvins.fr/",  "logo"=> "logo_igpcharentais.png"
+    ),
+    "comtetolosan" => array (
+        "title" => "Fédération Régionale des vins IGP du Sud-Ouest<br/><small>(Ariège, Aveyron, Comté Tolosan, Lavilledieu, Thezac Perricard)</small>",
+        "adresse" => "BP 82256 – Auzeville<br/>31322 Castanet Tolosan",
+        "contact" => "05.61.75.42.82<br/>s.arenes@coopoccitanie.fr<br/><b>Contact:</b> Sandrine ARENES DABAN<br/><b>Président :</b> Christophe Bou<br/>",
+        "portail"=>"https://sudouest.igp.vins.24eme.fr/",  "logo"=> "logo_igpsudouest.png"
+
+    ),
+    "drome" => array (
+        "title" => "Federation Dromoise des IGP viticoles",
+        "adresse" => "Bureau Viti 26 - Immeuble CER FRANCE Drome Vaucluse<br/>ZA des Grands Crus - 1575, chemin des Levees<br/>26600 TAIN l'HERMITAGE",
+        "contact" => "04 75 07 80 06 - 06 12 02 41 03<br/><br/>viti26@orange.fr<br/><br/><b>Contact:</b> Juliette VIOLETTE<br/><b>Président:</b> Adelin MARCHAUD<br/>",
+        "portail"=>"https://drome.igp.vins.24eme.fr/",  "logo"=> "logo_igpdrome.png"
+    ),
+    "gard" => array (
+        "title" => "Federation Gardoise des Vins à IGP",
+        "adresse" => "Le Solaris<br/>545, chemin du Chai<br/>30900 NIMES",
+        "contact" => "04 66 06 39 22<br/>mireille.compan@odg30.fr<br/><br/><b>Contact:</b> Mireille COMPAN<br/><b>Président:</b> Denis VERDIER<br/>",
+        "portail"=>"https://www.innov-agro.igpvins.fr/",  "logo"=> "logo_igpgard.png"
+    ),
+    "gascogne" => array (
+        "title" => "Syndicat des vins Cotes de Gascogne et Gers",
+        "adresse" => "Route de Cazaubon 1286, av de l′Armagnac<br/>BP 2<br/>32800 EAUZE",
+        "contact" => "05.62.09.82.19<br/>alain@vins-cotes-gascogne.fr<br/><a href='http://www.vins-cotes-gascogne.fr'>Site web</a><br/><b>Contact:</b> Alain DESPRATS<br/><b>Président:</b> Jean-Pierre DRIEUX<br/>",
+        "portail"=>"https://gascogne.igp.vins.24eme.fr/",  "logo"=> "logo_igpgascogne.png"
+    ),
+    "herault" => array (
+        "title" => "Federation<br/>IGP 34",
+        "adresse" => "Rond point de la Vierge Maurin BP 20006<br/><br/>34871 LATTES Cedex",
+        "contact" => "04 67 07 03 26<br/>s.olivet@vignerons.com<br/><br/><b>Contact:</b> Sylvie OLIVET<br/><b>Président:</b> Jean Michel SAGNIER<br/>",
+        'portail' => "https://www.innov-agro.igpvins.fr/", "logo" => "logo_igpherault.png"
+    ),
+    "loire" => array (
+        "title" => "Syndicat des Vins IGP du Val de Loire",
+        "adresse" => "6 Rue du Maine<br/><br/>49100 ANGERS",
+        "contact" => "02 41 87 25 81<br/>contact@igpvaldeloire.fr<br/><a href='http://www.vinsdepays-valdeloire.com'>Site web</a><br/><b>Contact:</b> Raphaël Fattier<br/><b>Présidente:</b> Catherine Motheron",
+        "portail"=>"https://loire.igp.vins.24eme.fr/",  "logo"=> "logo_igploire.png"
+    ),
+    "tarn" => array (
+        "title" => "Syndicat des vins IGP Côtes du Tarn ",
+        "adresse" => "Maison des Vins – Abbaye Saint Michel<br/><br/>81600 GAILLAC",
+        "contact" => "05 63 57 15 40<br/>cotesdutarn@vins-gaillac.com<br/><a href='http://www.vins-gaillac.com'>Site web</a><br/><b>Contact:</b> Caroline Granado<br/><b>Président:</b> Cédric Carcenac",
+        "portail"=>"https://teledeclaration.vins-gaillac.com/",  "logo"=> "logo_igptarn.png"
+    ),
+    "var" => array (
+        "title" => "Syndicat des Vignerons<br/>du Var",
+        "adresse" => "70 Avenue du President Wilson<br/><br/>83550 VIDAUBAN",
+        "contact" => "04 94 73 54 12<br/>jrouzier.svv@orange.fr<br/><br/><b>Contact:</b> Emmanuelle RANC<br/><b>Président:</b> Eric PAUL<br/>",
+        "portail"=>"https://var.igp.vins.24eme.fr/",  "logo"=> "logo_igpvar.png"
+    ),
+    "vaucluse" => array (
+        "title" => "Syndicat des Vins IGP Vaucluse",
+        "adresse" => "35 Chemin de Saint Genies<br/><br/>84210 VENASQUE",
+        "contact" => "04 90 12 45 20<br/>contact@vins-igp-vaucluse.fr<br/><a href='http://www.vins-igp-vaucluse.fr'>Site web</a><br/><b>Contact:</b> Mladen GRUMIC<br/><b>Président:</b> Joel REYNAUD<br/>",
+        "portail"=>"https://vaucluse.igp.vins.24eme.fr/",  "logo"=> "logo_igpvaucluse.png"
+    ),
+    "med" => array (
+        "title" => "Fédération<br/>INTER-MED",
+        "adresse" => "<br/>198 chemin des Entrages<br/>13300 SALON-DE-PROVENCE",
+        "contact" => "06 22 50 73 29<br/>contact@igpmed.fr<br/><a href='http://www.igpmed.fr'>Site web</a><br/><b>Contact:</b> Axelle FICHTNER<br/><b>Président:</b> Thierry ICARD<br/>",
+        "portail"=>"https://med.igp.vins.24eme.fr/", "logo"=> "logo_igpmed.png"
+    ),
+    "terredumidi" => array (
+        "title" => "Syndicat des Producteurs de Terres du midi",
+        "adresse" => "Domaine de Manse - Avenue Paysagere<br/>CS 70026 - Maurin<br/>34973 LATTES",
+        "contact" => "04 67 82 19 36<br/>accueil@igpterresdumidi.fr<br/><br/><b>Contacts dossiers :</b><ul><li><b>Aude et P.O.</b> : Laurence ZORZETTIG l.zorzettig@vignerons.com 04.68.90.22.29</li><li><b>Gard : </b>Mireille COMPAN m.compan@vignerons.com 04.66.70.94.58</li><li><b>Herault :</b> Sylvie OLIVET s.olivet@vignerons.com 04 67 07 03 26</li></ul><br/><b>Président:</b> Ludovic ROUX<br/>",
+        "portail" => "#", "logo" => "logo_igpterredumidi.png"
+    )
+);
+
+$igp_documents = array();
+
+$type_documents = array(
+        "TEXTEOFFICIEL" => "Textes officiels",
+        "FORMULAIREDECLARATIF" =>"Formulaires déclaratifs",
+        "AUTREDOCUMENT" => "Autres documents",
+);
+
+foreach($igp_contacts as $igp => $details) {
+    $docs = array();
+    global $docs;
+    if (is_dir('documents/'.$igp)) {
+      foreach (scandir('documents/'.$igp) as $file) {
+        if (preg_match('/([0-9-]*)_([A-Z]*)_(.*)(.pdf|.doc)/', $file, $m)) {
+            if (isset($type_documents[$m[2]])) {
+                $docs[] = array($type_documents[$m[2]], $m[1], $m[3], $m[0]);
+            }
+        }
+      }
+    }
+    uksort($docs, function ($b, $a) { global $docs;return strcmp($docs[$a][0].$docs[$a][1].$docs[$a][2], $docs[$b][0].$docs[$b][1].$docs[$b][2]); } );
+    $igp_documents[$igp] = $docs;
+}
--- igp.php
+++ igp.php
@@ -1,131 +1,138 @@
-<?php
-$igp_contacts = array(
-    "allobrogies" => array (
-        "title" => "Syndicat des Vins de Pays Allobrogies",
-        "adresse" => "40 rue du Terraillet<br/><br/>73190 SAINT BALDOPH",
-        "contact" => "04 79 33 82 42<br/>emmanuel.michaud@fdsea73.fr<br/><br/><b>Contact:</b>Emmanuel MICHAUD<br/><b>Président:</b>Jean-Paul FINAS<br/>",
-        "portail" => "https://www.innov-agro.igpvins.fr/", "logo" => "logo_igpallobrogies.png"
-    ),
-    "alpesdusud" => array(
-        "title" => "Syndicat des Vins des Alpes du Sud",
-        "adresse" => "2 rue Osco Manosco<br/><br/>04860 PIERREVERT",
-        "contact" => "04 90 12 45 20<br/>direction@vins-des-alpes-du-sud.fr<br/><br/><b>Contact : </b>Mladen GRUMIC<br/><b>Président AOP :</b> Gilles DELSUC<br/><b>Président IGP :</b>  Frédéric PORT<br/>",
-        "portail"=>"https://alpes.igp.vins.24eme.fr/",  "logo"=> "logo_igpalpes.png"
-    ),
-    "ardeche" =>  array (
-        "title" => "Syndicat des Vins IGP d′Ardeche",
-        "adresse" => "Maison des Vignerons<br />218 Route de Ruoms<br/><br/>07150 VALLON PONT D'ARC",
-        "contact" => "04 75 93 90 13<br/>svdp-fcca@wanadoo.fr<br/><a href='http://www.lesvinsdardeche.com'>Site web</a><br/><b>Contact:</b> Catherine JURY - Anne MOISAN<br/><b>Président :</b> Marc DEJOUX<br/>",
-        "portail"=>"https://ardeche.igp.vins.24eme.fr/",  "logo"=> "logo_igpardeche.png"
-    ),
-    "aude" => array (
-        "title" => "Union Syndicale des IGP de l′Aude",
-        "adresse" => "Maison des Vignerons - ZAC Bonne Source<br/>22 rue Ernest Cognacq - BP 40527<br/>11105 NARBONNECedex",
-        "contact" => "04.68.90.22.29<br/>l.zorzettig@vignerons.com<br/><br/><b>Contact:</b> Laurence ZORZETTIG<br/><b>Président:</b> Ludovic ROUX<br/>",
-        "portail"=>"https://www.innov-agro.igpvins.fr/", "logo" => "logo_igpaude.png"
-    ),
-    "13" => array (
-        "title" => "Syndicat des vins IGP des Bouches-du-Rhone",
-        "adresse" => "Maison des Agriculteurs<br/>22 Avenue Henri-Pontier<br/>13626 AIX EN PROVENCE Cedex 1",
-        "contact" => "06.23.90.24.41<br/>igp13@orange.fr<br/><br/><b>Contact:</b> Marie DE MONTE<br/><b>Président:</b> Jean-Claude PELLEGRIN<br/>",
-        "portail"=> array('Antenne d\'Aix' => "https://13.igp.vins.24eme.fr/", 'Antenne d\'Arles' => "https://arles.igp.vins.24eme.fr/"),  "logo" => "logo_igp13.png",
-        "url_plusdinfo" => "https://www.igp13vins.com/documents-officiels-2.html",
-    ),
-    "charentais" => array (
-        "title" => "Syndicat des Vins de Pays Charentais",
-        "adresse" => "23 allees Bernard Guionnet - BP 90018<br/><br/>16101 COGNAC CEDEX",
-        "contact" => "05 17 22 00 00<br/>info@vindepayscharentais.fr<br/><a href='http://www.vindepayscharentais.fr'>Site web</a><br/><b>Contact:</b> Julie FOUASSIER<br/><b>Président:</b> Thierry JULLION<br/>",
-        "portail"=>"https://www.innov-agro.igpvins.fr/",  "logo"=> "logo_igpcharentais.png"
-    ),
-    "comtetolosan" => array (
-        "title" => "Fédération Régionale des vins IGP du Sud-Ouest<br/><small>(Ariège, Aveyron, Comté Tolosan, Lavilledieu, Thezac Perricard)</small>",
-        "adresse" => "BP 82256 – Auzeville<br/>31322 Castanet Tolosan",
-        "contact" => "05.61.75.42.82<br/>s.arenes@coopoccitanie.fr<br/><b>Contact:</b> Sandrine ARENES DABAN<br/><b>Président :</b> Christophe Bou<br/>",
-        "portail"=>"https://sudouest.igp.vins.24eme.fr/",  "logo"=> "logo_igpsudouest.png"
+<?php include('config_igp.php'); ?><!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="La plateforme de télédéclarations de IGP ">
+    <meta name="author" content="IGP & 24eme">
 
-    ),
-    "drome" => array (
-        "title" => "Federation Dromoise des IGP viticoles",
-        "adresse" => "Bureau Viti 26 - Immeuble CER FRANCE Drome Vaucluse<br/>ZA des Grands Crus - 1575, chemin des Levees<br/>26600 TAIN l'HERMITAGE",
-        "contact" => "04 75 07 80 06 - 06 12 02 41 03<br/><br/>viti26@orange.fr<br/><br/><b>Contact:</b> Juliette VIOLETTE<br/><b>Président:</b> Adelin MARCHAUD<br/>",
-        "portail"=>"https://drome.igp.vins.24eme.fr/",  "logo"=> "logo_igpdrome.png"
-    ),
-    "gard" => array (
-        "title" => "Federation Gardoise des Vins à IGP",
-        "adresse" => "Le Solaris<br/>545, chemin du Chai<br/>30900 NIMES",
-        "contact" => "04 66 06 39 22<br/>mireille.compan@odg30.fr<br/><br/><b>Contact:</b> Mireille COMPAN<br/><b>Président:</b> Denis VERDIER<br/>",
-        "portail"=>"https://www.innov-agro.igpvins.fr/",  "logo"=> "logo_igpgard.png"
-    ),
-    "gascogne" => array (
-        "title" => "Syndicat des vins Cotes de Gascogne et Gers",
-        "adresse" => "Route de Cazaubon 1286, av de l′Armagnac<br/>BP 2<br/>32800 EAUZE",
-        "contact" => "05.62.09.82.19<br/>alain@vins-cotes-gascogne.fr<br/><a href='http://www.vins-cotes-gascogne.fr'>Site web</a><br/><b>Contact:</b> Alain DESPRATS<br/><b>Président:</b> Jean-Pierre DRIEUX<br/>",
-        "portail"=>"https://gascogne.igp.vins.24eme.fr/",  "logo"=> "logo_igpgascogne.png"
-    ),
-    "herault" => array (
-        "title" => "Federation<br/>IGP 34",
-        "adresse" => "Rond point de la Vierge Maurin BP 20006<br/><br/>34871 LATTES Cedex",
-        "contact" => "04 67 07 03 26<br/>s.olivet@vignerons.com<br/><br/><b>Contact:</b> Sylvie OLIVET<br/><b>Président:</b> Jean Michel SAGNIER<br/>",
-        'portail' => "https://www.innov-agro.igpvins.fr/", "logo" => "logo_igpherault.png"
-    ),
-    "loire" => array (
-        "title" => "Syndicat des Vins IGP du Val de Loire",
-        "adresse" => "6 Rue du Maine<br/><br/>49100 ANGERS",
-        "contact" => "02 41 87 25 81<br/>contact@igpvaldeloire.fr<br/><a href='http://www.vinsdepays-valdeloire.com'>Site web</a><br/><b>Contact:</b> Raphaël Fattier<br/><b>Présidente:</b> Catherine Motheron",
-        "portail"=>"https://loire.igp.vins.24eme.fr/",  "logo"=> "logo_igploire.png"
-    ),
-    "tarn" => array (
-        "title" => "Syndicat des vins IGP Côtes du Tarn ",
-        "adresse" => "Maison des Vins – Abbaye Saint Michel<br/><br/>81600 GAILLAC",
-        "contact" => "05 63 57 15 40<br/>cotesdutarn@vins-gaillac.com<br/><a href='http://www.vins-gaillac.com'>Site web</a><br/><b>Contact:</b> Caroline Granado<br/><b>Président:</b> Cédric Carcenac",
-        "portail"=>"https://teledeclaration.vins-gaillac.com/",  "logo"=> "logo_igptarn.png"
-    ),
-    "var" => array (
-        "title" => "Syndicat des Vignerons<br/>du Var",
-        "adresse" => "70 Avenue du President Wilson<br/><br/>83550 VIDAUBAN",
-        "contact" => "04 94 73 54 12<br/>jrouzier.svv@orange.fr<br/><br/><b>Contact:</b> Emmanuelle RANC<br/><b>Président:</b> Eric PAUL<br/>",
-        "portail"=>"https://var.igp.vins.24eme.fr/",  "logo"=> "logo_igpvar.png"
-    ),
-    "vaucluse" => array (
-        "title" => "Syndicat des Vins IGP Vaucluse",
-        "adresse" => "35 Chemin de Saint Genies<br/><br/>84210 VENASQUE",
-        "contact" => "04 90 12 45 20<br/>contact@vins-igp-vaucluse.fr<br/><a href='http://www.vins-igp-vaucluse.fr'>Site web</a><br/><b>Contact:</b> Mladen GRUMIC<br/><b>Président:</b> Joel REYNAUD<br/>",
-        "portail"=>"https://vaucluse.igp.vins.24eme.fr/",  "logo"=> "logo_igpvaucluse.png"
-    ),
-    "med" => array (
-        "title" => "Fédération<br/>INTER-MED",
-        "adresse" => "<br/>198 chemin des Entrages<br/>13300 SALON-DE-PROVENCE",
-        "contact" => "06 22 50 73 29<br/>contact@igpmed.fr<br/><a href='http://www.igpmed.fr'>Site web</a><br/><b>Contact:</b> Axelle FICHTNER<br/><b>Président:</b> Thierry ICARD<br/>",
-        "portail"=>"https://med.igp.vins.24eme.fr/", "logo"=> "logo_igpmed.png"
-    ),
-    "terredumidi" => array (
-        "title" => "Syndicat des Producteurs de Terres du midi",
-        "adresse" => "Domaine de Manse - Avenue Paysagere<br/>CS 70026 - Maurin<br/>34973 LATTES",
-        "contact" => "04 67 82 19 36<br/>accueil@igpterresdumidi.fr<br/><br/><b>Contacts dossiers :</b><ul><li><b>Aude et P.O.</b> : Laurence ZORZETTIG l.zorzettig@vignerons.com 04.68.90.22.29</li><li><b>Gard : </b>Mireille COMPAN m.compan@vignerons.com 04.66.70.94.58</li><li><b>Herault :</b> Sylvie OLIVET s.olivet@vignerons.com 04 67 07 03 26</li></ul><br/><b>Président:</b> Ludovic ROUX<br/>",
-        "portail" => "#", "logo" => "logo_igpterredumidi.png"
-    )
-);
+    <title>OpenODG : IGP</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+   <style>
+   <!--
+.bg-dark{ background-color: #af588f !important; }
 
-$igp_documents = array();
-
-$type_documents = array(
-        "TEXTEOFFICIEL" => "Textes officiels",
-        "FORMULAIREDECLARATIF" =>"Formulaires déclaratifs",
-        "AUTREDOCUMENT" => "Autres documents",
-);
-
-foreach($igp_contacts as $igp => $details) {
-    $docs = array();
-    global $docs;
-    if (is_dir('documents/'.$igp)) {
-      foreach (scandir('documents/'.$igp) as $file) {
-        if (preg_match('/([0-9-]*)_([A-Z]*)_(.*)(.pdf|.doc)/', $file, $m)) {
-            if (isset($type_documents[$m[2]])) {
-                $docs[] = array($type_documents[$m[2]], $m[1], $m[3], $m[0]);
-            }
-        }
-      }
-    }
-    uksort($docs, function ($b, $a) { global $docs;return strcmp($docs[$a][0].$docs[$a][1].$docs[$a][2], $docs[$b][0].$docs[$b][1].$docs[$b][2]); } );
-    $igp_documents[$igp] = $docs;
+.btn-primary:hover {
+    color: #fff;
+    background-color: #af588f;
+    border-color: #fff;
 }
+
+.btn-primary {
+    color: #fff;
+    background-color: #af588f;
+    border-color: #af588f;
+}
+
+#navbarHeader .text-muted {
+    color: #ec95cc !important;
+}
+   -->
+   </style>
+  <body>
+
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">Portail des Vins IGP</h4>
+              <p class="text-muted">Retrouver depuis cette pages les adresses de tous les portails et documents des Syndicats viticoles des IGP</p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white"></h4>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white"></a></li>
+                <li><a href="#" class="text-white"></a></li>
+                <li><a href="#" class="text-white"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="/" class="navbar-brand d-flex align-items-center">
+            <strong>Portail des Vins IGP</strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+
+    <main role="main">
+
+      <section class="jumbotron text-center">
+        <div class="container">
+          <img class="jumbotron-heading" src="/images/igp.png" style="height: 200px" alt="Logo IGP"/>
+          <h1 class="jumbotron-heading">Vins IGP</h1>
+          <p class="lead text-muted">Retrouver depuis cette pages les adresses de tous les portails et documents des Syndicats viticoles des IGP.</p>
+          <p>
+<!--
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+-->
+          </p>
+        </div>
+      </section>
+
+      <div class="album py-5 bg-light">
+      <div class="container">
+      <div class="row">
+<?php foreach($igp_contacts as $igp => $params): ?>
+            <div class="col-md-4">
+              <div class="card mb-4 box-shadow">
+                <div class="card-body">
+                  <div style="height: 225px;" class="d-flex">
+                      <img class="mx-auto align-self-center" src="images/logos/<?php echo $params['logo']; ?>" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=<?php echo $params['title']; ?>" alt="Logo <?php echo $params['title']; ?>"  style="max-height: 225px;">
+                  </div>
+                  <h5 class="py-1 px-3 card-title text-center"><?php echo $params['title']; ?></h5>
+                  <?php if (is_array($params['portail'])): ?>
+                  <div class="row">
+                  <?php foreach ($params['portail'] as $nom => $url ): ?>
+                  <div class="col-sm-6">
+                  <a href="<?php echo $url; ?>" class="btn btn-block btn-primary"><?php echo $nom; ?></a>
+                  </div>
+                  <?php endforeach; ?>
+                  </div>
+                  <?php else: ?>
+                    <div class="d-flex">
+                    <a href="<?php echo $params['portail']; ?>" class="btn btn-block btn-primary">Accéder au portail</a>
+                    </div>
+                  <?php endif; ?>
+                </div>
+                <div class="card-footer text-center">
+                    <a class="text-muted" href="plus.php?igp=<?php echo $igp; ?>">Plus d'info</a>
+                </div>
+              </div>
+            </div>
+<?php endforeach; ?>
+      </div>
+      </div>
+      </div>
+
+    </main>
+
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="https://vinigp.fr/">Confédération des vins IGP de France</a>
+        </p>
+        <p>Portail de télédéclaration des syndicats viticoles des IGP.</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.0/holder.js"></script>
+  </body>
+</html>
